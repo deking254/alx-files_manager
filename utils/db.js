81126e02aa781a07c37e5937ab506cbc0845d9dd
deking254 <dekingsky@gmail.com>
2024-02-20 14:18:31 +0000

    code for the file 2

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+const env = require('process').env;
+class DBClient {
+  constructor() {
+    this.clientObject = null;
+    if (env.DB_DATABASE){
+      this.databaseName = env.DB_DATABASE;
+    } else {
+      this.databaseName = 'files_manager';
+    }
+    if (env.DB_PORT && env.DB_HOST && env.DB_DATABASE){ 
+      this.url = `mongodb://${env.DB_HOST}:${env.DB_PORT}/${env.DB_DATABASE}`
+    }else{
+      this.url = `mongodb://localhost:27017/file_manager`;
+    }
+    this.client = new MongoClient(this.url);
+    }
+  isAlive() {
+    this.client.connect((error, client)=>{
+      if (error){
+        return false
+      } else {
+        return true
+      }
+    })
+  }
+  async nbUsers (){
+    if (this.isAlive()){
+      
+    }
+  }
+  async nbFiles () {
+
+  }
+}
+const dbClient = new DBClient();
+module.exports = dbClient;
