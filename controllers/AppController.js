fa367de6568ec05207a2196f79429050a2365914
deking254 <dekingsky@gmail.com>
2024-02-22 11:32:02 +0000

    removed the json

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -7,17 +7,16 @@ let fileCount;
 async function getStats () {
   userCount = await mongoClient.nbUsers();
   fileCount = await mongoClient.nbFiles();
+  router.get('/stats', (req, res)=>{
+    res.statusCode = 200;
+    let result = { "users": userCount, "files": fileCount };
+    res.status(200).send(result);
+  })
 }
 getStats();
 router.get('/status', (req, res)=>{
   res.statusCode = 200;
   let result = { "redis": redisClient.isAlive(), "db": mongoClient.isAlive() };
-  res.status(200).send(result);
-})
-
-router.get('/stats', (req, res)=>{
-  res.statusCode = 200;
-  let result = { "users": userCount, "files": fileCount };
   res.status(200).send(result);
 })
 module.exports = router;
